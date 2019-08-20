a34bfccca3b6df9cf1413f8ffb392028619cf439
vincent servio <vincentservio@gmail.com>
2019-08-20 04:14:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   a= ['I','am','really','learning','arrays!']
-#pop 'arrays'
+#pop 'arrays!'
 end
 
 def remove_element_from_start_of_array(array)
